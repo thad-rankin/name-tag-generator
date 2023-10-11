9bdc541ef8227c9419a0f08a0526c80491796bad
Thad R <122906384+thad-rankin@users.noreply.github.com>
2023-10-11 12:24:29 -0500

    Updated code letting users delete name tags, built text and button to let users input text

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import NameTag from "./NameTag";
+import App from "./App.js";
+
 import "./styles.css";
-
-const renderNameTag = (name) => <NameTag name={name} key={name} />;
-
-class App extends Component {
-
-  state = { names: ["Thad", "Meghan", "Taylor", "Cameron", "Jenny", "Justine", "James"]}
-
-  render(){
-    const NameTagElements = this.state.names.map(renderNameTag);
-
-    return (
-    <div className="App">
-      <h1>Name Tag Generator</h1>
-      {NameTagElements}
-    </div>
-    );    
-  }
-};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
